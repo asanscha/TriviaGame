c4a3ae8783e930f47b1463ed8cc59df3fd9afc1c
asanscha <50388800+asanscha@users.noreply.github.com>
2019-07-27 15:03:06 -0700

    Add files via upload

---
+++ TriviaGame/assets/javascript/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+
+
+    const quizQuestions = [
+        {
+          question: "What is the name of Batman's butler?",
+          answers: {
+            a: "Robin",
+            b: "Alfred",
+            c: "Charles"
+          },
+          correctAnswer: "b"
+        },
+        {
+          question: "Hg is the chemical symbol of which element?",
+          answers: {
+            a: "Mercury",
+            b: "Hassium",
+            c: "Iron"
+          },
+          correctAnswer: "a"
+        },
+        {
+          question: "Where is Waldo really?",
+          answers: {
+            a: "Antarctica",
+            b: "Exploring the Pacific Ocean",
+            c: "Sitting in a tree",
+            d: "Minding his own business"
+          },
+          correctAnswer: "d"
+        },
+        {
+          question: "Who was the US president during World War I?",
+          answers: {
+            a: "Theodore Roosevelt",
+            b: "Woodrow Wilson",
+            c: "William H Taft",
+            d: "Calvin Coolidge"
+          },
+          correctAnswer: "b"
+        },
+        {
+          question: "Which is the highest waterfall in the world?",
+          answers: {
+              a: "Niagara Falls",
+              b: "Angel Falls",
+              c: "Tugela Falls"
+          },
+          correctAnswer: "b"    
+        },
+          
+      ];
+
+
+
+  });
